e63d96c7671b71f8fda461755825e81e306cb489
Nathaniel D'Rozario <nsdrozario@gmail.com>
2019-07-13 16:18:49 -0700

    fixed vector insertion bug, the file creation method works now
    "

--- ndcmd_img.cpp
+++ ndcmd_img.cpp
@@ -6,7 +6,14 @@
 
 nd_cmd_paint::image_data::image_data(int sx, int sy)
     : size_x(sx), size_y(sy)
-{}
+{
+    for (int y=0; y<sy; y++) {
+        this->data.push_back(std::vector<int>()); // default value
+        for (int x=0; x<sx; x++) {
+        this->data[y].push_back(0); 
+        }
+    }
+}
 nd_cmd_paint::Pixel* nd_cmd_paint::image_data::get_pixel(int x, int y) {
     nd_cmd_paint::Pixel *p = new nd_cmd_paint::Pixel(x,y,0);
     p->color = this->data[y][x];
--- paint.cpp
+++ paint.cpp
@@ -7,8 +7,13 @@
 
 int main(int argc, char *argv[]) {
     std::cout << "Entry point" << std::endl;
-    nd_cmd_paint::image_data *img = new nd_cmd_paint::image_data(1,1); // this is just test code
-    img->place_pixel(0,0,1);
+    nd_cmd_paint::image_data *img = new nd_cmd_paint::image_data(5,5); // this is just test code
+    for (int y=0; y<5; y++) {
+        for (int x=0; x<5; x++) {
+            img->place_pixel(x,y,1);
+        }
+    }
+
     
     std::cout << "Placed pixel in image data" << std::endl;
 
